5ae7230aee565969b1143c88da18d343228fb5e4
blueyi <blueyiniu@qq.com>
2015-07-30 09:01:57 +0800

    Add time statistic

--- 7_point_stencil_jacobi3d.cu
+++ 7_point_stencil_jacobi3d.cu
@@ -16,7 +16,7 @@ const int SIZE = dimX * dimY * dimZ;
 const int threadPerBlock = 32;
 
 //设置迭代次数
-const int times = 2;
+const int times = 10;
 
 //设置stencil边界处邻居的值
 __device__ const float BORDER = 0.0;
@@ -27,12 +27,12 @@ int count = 0;
 //设定线程格中线程块的数量, 避免启动过多线程块
 int blockPerGrid(const int dim, const int threadPerBlock)
 {
-    //由于暂时一个线程只计算一个stencil，所以暂时不能指定线程块的限制
-    int temp = dim / threadPerBlock;
-    if (dim % threadPerBlock != 0) {
-       temp += 1; 
-    }
-    return temp;
+   //由于暂时一个线程只计算一个stencil，所以暂时不能指定线程块的限制
+   int temp = dim / threadPerBlock;
+   if (dim % threadPerBlock != 0) {
+      temp += 1; 
+   }
+   return temp;
 }
 
 //错误处理
@@ -41,137 +41,157 @@ int blockPerGrid(const int dim, const int threadPerBlock)
 
 inline void checkCudaError(cudaError_t error, const char *file, const int line)
 {
-    if (error != cudaSuccess) {
-        std::cerr << "CUDA CALL FAILED:" << file << "( " << line << ")- " << cudaGetErrorString(error) << std::endl;
-        exit(EXIT_FAILURE);
-    }
+   if (error != cudaSuccess) {
+      std::cerr << "CUDA CALL FAILED:" << file << "( " << line << ")- " << cudaGetErrorString(error) << std::endl;
+      exit(EXIT_FAILURE);
+   }
 }
 inline void checkCudaState(const char *msg, const char *file, const int line)
 {
-    cudaError_t error = cudaGetLastError();
-    if (error != cudaSuccess) {
-        std::cerr << "---" << msg << " Error---" << std::endl;
-        std::cerr << file << "( " << line << ")- " << cudaGetErrorString(error) << std::endl;
-        exit(EXIT_FAILURE);
-    }
+   cudaError_t error = cudaGetLastError();
+   if (error != cudaSuccess) {
+      std::cerr << "---" << msg << " Error---" << std::endl;
+      std::cerr << file << "( " << line << ")- " << cudaGetErrorString(error) << std::endl;
+      exit(EXIT_FAILURE);
+   }
 }
 
 
 //计算线程与元素的唯一偏移，以x为行，y为列，z为高
 __device__ __host__ int offset(int x, int y, int z) 
 {
-    return (((x + dimX) % dimX) + ((y + dimY) % dimY) * dimX + ((z + dimZ) % dimZ) * dimX * dimY);
+   return (((x + dimX) % dimX) + ((y + dimY) % dimY) * dimX + ((z + dimZ) % dimZ) * dimX * dimY);
 }
 
 __global__ void kernel(double *dev_grid_in, double *dev_grid_out)
 {
-    int x = threadIdx.x + blockIdx.x * blockDim.x;
-    int y = threadIdx.y + blockIdx.y * blockDim.y;
-    int z = threadIdx.z + blockIdx.z * blockDim.z;
-    
-    //设置stencil中各元素值
-    int center = dev_grid_in[offset(x, y, z)];
-    int up     = (z < (dimZ - 1)) ? dev_grid_in[offset(x, y, z + 1)] : BORDER;
-    int down   = (z > 0) ? dev_grid_in[offset(x, y, z - 1)] : BORDER;
-    int west   = (x > 0) ? dev_grid_in[offset(x - 1, y, z)] : BORDER;
-    int east   = (x < (dimX - 1)) ? dev_grid_in[offset(x + 1, y, z)] : BORDER;
-    int south  = (y > 0) ? dev_grid_in[offset(x, y - 1, z)] : BORDER;
-    int north  = (y < (dimY - 1)) ? dev_grid_in[offset(x, y + 1, z)] : BORDER;
+//   __shared__ double cache[threadPerBlock][threadPerBlock][1];
+   int x = threadIdx.x + blockIdx.x * blockDim.x;
+   int y = threadIdx.y + blockIdx.y * blockDim.y;
+   int z = threadIdx.z + blockIdx.z * blockDim.z;
 
-//    dev_grid_out[offset(x, y, z)] = 1.0;
+   //设置stencil中各元素值
+   int center = dev_grid_in[offset(x, y, z)];
+   int up     = (z < (dimZ - 1)) ? dev_grid_in[offset(x, y, z + 1)] : BORDER;
+   int down   = (z > 0) ? dev_grid_in[offset(x, y, z - 1)] : BORDER;
+   int west   = (x > 0) ? dev_grid_in[offset(x - 1, y, z)] : BORDER;
+   int east   = (x < (dimX - 1)) ? dev_grid_in[offset(x + 1, y, z)] : BORDER;
+   int south  = (y > 0) ? dev_grid_in[offset(x, y - 1, z)] : BORDER;
+   int north  = (y < (dimY - 1)) ? dev_grid_in[offset(x, y + 1, z)] : BORDER;
+
+   //    dev_grid_out[offset(x, y, z)] = 1.0;
    dev_grid_out[offset(x, y, z)] = (center + up + down + west + east + south + north) * (1.0 / 7.0);
+//   cache[threadIdx.x][threadIdx.y][threadIdx.z] = (center + up + down + west + east + south + north) * (1.0 / 7.0);
+//   __syncthreads();
+//   dev_grid_out[offset(x, y, z)] = [threadIdx.x][threadIdx.y][threadIdx.z];
+
 }
 
 //初始化输入，输出
 void init(double *grid, int dimX, int dimY, int dimZ)
 {
-    for (int z=0; z<dimZ; ++z) {
-        for (int y=0; y<dimY; ++y) {
-            for (int x=0; x<dimX; ++x) {
-                if ((x*y*z == 0) || (x == dimX-1) || (y == dimY-1) || (z == dimZ-1)) {
-                    grid[offset(x, y, z)] = 7;
-                }
-                else {
-                  grid[offset(x, y, z)] = 0;
-              //      grid[offset(x, y, z)] = count;
-                }
-                count++;
+   for (int z=0; z<dimZ; ++z) {
+      for (int y=0; y<dimY; ++y) {
+         for (int x=0; x<dimX; ++x) {
+            if ((x*y*z == 0) || (x == dimX-1) || (y == dimY-1) || (z == dimZ-1)) {
+               grid[offset(x, y, z)] = 7;
             }
-        }
-    }
+            else {
+               grid[offset(x, y, z)] = 0;
+               //      grid[offset(x, y, z)] = count;
+            }
+            count++;
+         }
+      }
+   }
 }
 
 void print(double *grid)
 {
-    for (int z=0; z<dimZ; ++z) {
-        std::cout << z << ":\n\n";
-        for (int y=0; y<dimY; ++y) {
-            for (int x=0; x<dimX; ++x) {
-                std::cout << std::fixed << std::setprecision(3) << grid[offset(x, y, z)] << "\t";
-            }
-            std::cout << std::endl;
-        }
-        std::cout << std::endl;
-    }
+   for (int z=0; z<dimZ; ++z) {
+      std::cout << z << ":\n\n";
+      for (int y=0; y<dimY; ++y) {
+         for (int x=0; x<dimX; ++x) {
+            std::cout << std::fixed << std::setprecision(3) << grid[offset(x, y, z)] << "\t";
+         }
+         std::cout << std::endl;
+      }
+      std::cout << std::endl;
+   }
 }
 
 void debug(int test, std::string str)
 {
 
-    if (test != 0) {
-        std::cout << "-----------" << str  << "--------------" << std::endl;
-        std::cout << test << std::endl;
-    }
-    else {
-        std::cout << "-----------" << str  << "--------------" << std::endl;
-    }
+   if (test != 0) {
+      std::cout << "-----------" << str  << "--------------" << std::endl;
+      std::cout << test << std::endl;
+   }
+   else {
+      std::cout << "-----------" << str  << "--------------" << std::endl;
+   }
 }
 
 int main(void)
 {
-    CHECK_ERROR(cudaSetDevice(0));
-    //此处为何不支持三维blockdim
-    dim3 blocks(threadPerBlock, threadPerBlock, 1);
-    dim3 grids(blockPerGrid(dimX, blocks.x), blockPerGrid(dimY, blocks.y), blockPerGrid(dimZ, blocks.z));
+   CHECK_ERROR(cudaSetDevice(0));
+   //由于blocks不能大于1024，所以最后一维设备为1
+   dim3 blocks(threadPerBlock, threadPerBlock, 1);
+   dim3 grids(blockPerGrid(dimX, blocks.x), blockPerGrid(dimY, blocks.y), blockPerGrid(dimZ, blocks.z));
 
-    double *grid_in, *grid_out;
-    grid_in = (double *)malloc(SIZE * sizeof(double));
-    grid_out = (double *)malloc(SIZE * sizeof(double));
+   double *grid_in, *grid_out;
+   grid_in = (double *)malloc(SIZE * sizeof(double));
+   grid_out = (double *)malloc(SIZE * sizeof(double));
 
-    double *dev_grid_in, *dev_grid_out;
-    CHECK_ERROR(cudaMalloc((void**)&dev_grid_in, SIZE * sizeof(double)));
-    CHECK_ERROR(cudaMalloc((void**)&dev_grid_out, SIZE * sizeof(double)));
+   double *dev_grid_in, *dev_grid_out;
+   CHECK_ERROR(cudaMalloc((void**)&dev_grid_in, SIZE * sizeof(double)));
+   CHECK_ERROR(cudaMalloc((void**)&dev_grid_out, SIZE * sizeof(double)));
 
-    init(grid_in, dimX, dimY, dimZ);
+   init(grid_in, dimX, dimY, dimZ);
+   init(grid_out, dimX, dimY, dimZ);
 
-    debug(0, "input");
-    print(grid_in);
+//   debug(0, "input");
+//   print(grid_in);
 
-    init(grid_out, dimX, dimY, dimZ);
+   //统计用于GPU计算的时间
+   cudaEvent_t start, stop;
+   CHECK_ERROR(cudaEventCreate(&start));
+   CHECK_ERROR(cudaEventCreate(&stop));
+   CHECK_ERROR(cudaEventRecord(start, 0));
+   CHECK_ERROR(cudaEventSynchronize(start));
 
-    CHECK_ERROR(cudaMemcpy(dev_grid_in, grid_in, SIZE * sizeof(double), cudaMemcpyHostToDevice));
-    CHECK_ERROR(cudaMemcpy(dev_grid_out, grid_out, SIZE * sizeof(double), cudaMemcpyHostToDevice));
+   CHECK_ERROR(cudaMemcpy(dev_grid_in, grid_in, SIZE * sizeof(double), cudaMemcpyHostToDevice));
+   CHECK_ERROR(cudaMemcpy(dev_grid_out, grid_out, SIZE * sizeof(double), cudaMemcpyHostToDevice));
 
-    cudaDeviceSynchronize();
+   for (int i=0; i<times; ++i) {
+      kernel<<<grids, blocks>>>(dev_grid_in, dev_grid_out);
+      std::swap(dev_grid_in, dev_grid_out);
+   }
+   cudaDeviceSynchronize();
 
-    for (int i=0; i<times; ++i) {
-        kernel<<<grids, blocks>>>(dev_grid_in, dev_grid_out);
-        std::swap(dev_grid_in, dev_grid_out);
-    }
-    cudaDeviceSynchronize();
+   CHECK_STATE("kernel call");
 
-    CHECK_STATE("kernel call");
+   CHECK_ERROR(cudaMemcpy(grid_in, dev_grid_in, SIZE * sizeof(double), cudaMemcpyDeviceToHost));
 
-    CHECK_ERROR(cudaMemcpy(grid_in, dev_grid_in, SIZE * sizeof(double), cudaMemcpyDeviceToHost));
+   //计算统计的时间
+   CHECK_ERROR(cudaEventRecord(stop, 0));
+   CHECK_ERROR(cudaEventSynchronize(stop));
+   float elapsedTime;
+   CHECK_ERROR(cudaEventElapsedTime(&elapsedTime, start, stop));
 
-    debug(0, "output");
-    print(grid_in);
+   debug(0, "output");
+   print(grid_in);
 
-    free(grid_in);
-    free(grid_out);
-    CHECK_ERROR(cudaFree(dev_grid_in));
-    CHECK_ERROR(cudaFree(dev_grid_out));
-    return 0;
+   std::cout << "Time elapsed: " << std::fixed << std::setprecision(6) << elapsedTime << " ms"  << std::endl;
+
+   CHECK_ERROR(cudaEventDestroy(start));
+   CHECK_ERROR(cudaEventDestroy(stop));
+
+   free(grid_in);
+   free(grid_out);
+   CHECK_ERROR(cudaFree(dev_grid_in));
+   CHECK_ERROR(cudaFree(dev_grid_out));
+   return 0;
 }
 
 
